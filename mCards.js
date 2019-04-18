8b5b15c616fef5e3e6fdf8d54abbba8ce88bd423
Victt0r <dsnobik@gmail.com>
2019-04-18 14:21:15 +0300

    splicedWord

--- mCards.js
+++ mCards.js
@@ -5,51 +5,49 @@ words = [['what', 'что'],
          ['which', 'который'], 
          ['who', 'кто'], 
          ['whose', 'чей'], 
+         ['colon', 'двоеточие'], 
          ['how', 'как']]
 
 function nextWord() {
   number = Math.floor(Math.random() * 8)
   ru_en = Math.floor(Math.random() * 2)
-  word.innerText = words[number][ru_en]
+  if (splicedWord) splicedWord = words.splice(number, 1, splicedWord)[0]
+  else splicedWord = words.splice(number, 1)[0]
+  word.innerText = splicedWord[ru_en]
+  
   input_form.value = ""
 }
+splicedWord = false
 counter_right = 0
 counter_wrong = 0
 nextWord()
 function inputHandler() {
   text = input_form.value
   console.log(text)
+  lekalo = splicedWord[1-ru_en]
 
-  if (words[number][0] == words[number][ru_en]) {lekalo = words[number][1]}
-  else if (words[number][1] == words[number][ru_en]) {lekalo = words[number][0]}
-  console.log(lekalo)
-
-  if (text == lekalo) {document.body.style.backgroundColor = "green";  window.setTimeout(nextWord, [1200]);
-  right.innerText = ++counter_right
+  if (text == lekalo) {
+    document.body.style.backgroundColor = "green";  
+    window.setTimeout(nextWord, [1200]);
+    right.innerText = ++counter_right
   }
-
-  if (text != lekalo) {document.body.style.backgroundColor = "red"; 
-  word.innerText = words[number][ru_en]+" = "+lekalo; 
-  wrong.innerText = ++counter_wrong
+  else {
+    document.body.style.backgroundColor = "red"; 
+    word.innerText = splicedWord[ru_en]+" = "+lekalo; 
+    wrong.innerText = ++counter_wrong
   }
 
 }
 input_form.onchange = inputHandler
 
 yes.onclick = function() {
-  if (words[number][0] == words[number][ru_en]) {lekalo = words[number][1]}
-  else if (words[number][1] == words[number][ru_en]) {lekalo = words[number][0]}
-
-  word.innerText = words[number][ru_en]+" = "+lekalo
+  word.innerText = splicedWord[ru_en]+" = "+splicedWord[1-ru_en]
   document.body.style.backgroundColor = "green"
-
   window.setTimeout(nextWord, [1500])
 }
 no.onclick = function() {
-  if (words[number][0] == words[number][ru_en]) {lekalo = words[number][1]}
-  else if (words[number][1] == words[number][ru_en]) {lekalo = words[number][0]}
- 
-  word.innerText = words[number][ru_en]+" = "+lekalo
+  word.innerText = splicedWord[ru_en]+" = "+splicedWord[1-ru_en]
   document.body.style.backgroundColor = "red"
 }
 // TODO не показывать подряд одно слово, ошибся/попал на каждом слове, серия без ошибки.
+// TODO качество UX 
