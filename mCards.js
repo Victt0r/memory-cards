50c0e617d612f18233f0e32c82316079219ae2b6
Victt0r <dsnobik@gmail.com>
2019-05-01 12:19:31 +0300

    refactoring

--- mCards.js
+++ mCards.js
@@ -12,76 +12,18 @@ words = [['what', 'что'],
          ['colon', 'двоеточие'], 
          ['how', 'как']]
 
-function nextWord() {
-  number = Math.floor(Math.random() * words.length)
-  ru_en = Math.floor(Math.random() * 2)
-  
-  splicedWord = splicedWord ? 
-    words.splice(number, 1, splicedWord)[0] : words.splice(number, 1)[0]
-  if (!splicedWord.hit) {splicedWord.hit = 0}
-  if (!splicedWord.miss) {splicedWord.miss = 0}
-
-  if (!localStorage[splicedWord[0]]) {localStorage[splicedWord[0]] = "0,0"}
-  arrLs = localStorage[splicedWord[0]].split(',')
-  splicedWord.hit = arrLs[0]
-
-  if (!localStorage[splicedWord[0]]) {localStorage[splicedWord[0]] = "0,0"}
-  arrLs = localStorage[splicedWord[0]].split(',')
-  splicedWord.miss = arrLs[1]
-  hitMiss.innerHTML = `<span id=hit>${splicedWord.hit}</span> :
-  <span id=miss>${splicedWord.miss}</span>`
-  word.innerText = splicedWord[ru_en]
-  input_form.value = ""
-}
 splicedWord = false
+
+// инициализация счетчиков
 right.innerText = counter_right = 
-  localStorage.counter_right ? +localStorage.counter_right : 0
+  localStorage.counter_right ? +localStorage.counter_right : 0 
 wrong.innerText = counter_wrong = 
   localStorage.counter_wrong ? +localStorage.counter_wrong : 0
 series.innerText = counter_series = 
   localStorage.counter_series? +localStorage.counter_series : 0
 
 nextWord()
-function inputHandler() {
-  text = input_form.value
-  lekalo = splicedWord[1-ru_en]
 
-  if (text == lekalo) {
-    // splicedWord.hit = splicedWord.hit ?splicedWord.hit+1 : 1
-
-    if (!localStorage[splicedWord[0]]) {localStorage[splicedWord[0]] = "0,0"}
-    arrLs = localStorage[splicedWord[0]].split(',')
-    splicedWord.hit = arrLs[0]
-    arrLs[0] = +arrLs[0]+1
-    console.log(arrLs)
-    localStorage[splicedWord[0]] = arrLs.join(',')
-
-    document.body.style.backgroundColor = "green";  
-    window.setTimeout(nextWord, [1200]);
-    right.innerText = ++counter_right
-    localStorage.counter_right = counter_right
-    series.innerText = ++counter_series
-    localStorage.counter_series = counter_series
-
-  }
-  else {
-    // splicedWord.miss = splicedWord.miss ? splicedWord.miss+1 : 1
-    if (!localStorage[splicedWord[0]]) {localStorage[splicedWord[0]] = "0,0"}
-    arrLs = localStorage[splicedWord[0]].split(',')
-    arrLs[1] = +arrLs[1]+1
-    console.log(arrLs)
-    localStorage[splicedWord[0]] = arrLs.join(',')
-
-    document.body.style.backgroundColor = "red"; 
-    word.innerText = splicedWord[ru_en]+" = "+lekalo; 
-    wrong.innerText = ++counter_wrong
-    localStorage.counter_wrong = counter_wrong
-    input_form.value = ""
-    localStorage.counter_series = series.innerText = counter_series = 0
-  }
-  hitMiss.innerHTML = `<span id=hit>${splicedWord.hit}</span> :
-  <span id=miss>${splicedWord.miss}</span>`
-}
 input_form.onchange = inputHandler
 
 yes.onclick = function() {
@@ -94,5 +36,57 @@ no.onclick = function() {
   document.body.style.backgroundColor = "red"
 }
 
-// TODO запись счетчиков hit|miss в LocalStorage
-// 
+function nextWord() {
+
+  // выбор случайной пары слов
+  number = Math.floor(Math.random() * words.length)
+  splicedWord = splicedWord ? 
+    words.splice(number, 1, splicedWord)[0] : words.splice(number, 1)[0]
+
+  // инициализация счетчиков
+  if (!splicedWord.hit) {splicedWord.hit = 0}
+  if (!splicedWord.miss) {splicedWord.miss = 0}
+  if (!localStorage[splicedWord[0]]) {localStorage[splicedWord[0]] = "0,0"}
+
+  // загрузка счетчиков
+  const arrHitMiss = localStorage[splicedWord[0]].split(',')
+  splicedWord.hit = arrHitMiss[0]
+  splicedWord.miss = arrHitMiss[1]
+  hitMiss.innerHTML = `<span id=hit>${splicedWord.hit}</span> :
+  <span id=miss>${splicedWord.miss}</span>`
+
+  // вывод слова на экран
+  ru_en = Math.floor(Math.random() * 2)
+  word.innerText = splicedWord[ru_en]
+  input_form.value = ""
+}
+function inputHandler() {
+  lekalo = splicedWord[1-ru_en]
+  const arrHitMiss = localStorage[splicedWord[0]].split(',')
+
+  if (input_form.value == lekalo) {
+    arrHitMiss[0] = +arrHitMiss[0]+1
+    splicedWord.hit = arrHitMiss[0]
+    document.body.style.backgroundColor = "green";  
+    localStorage.counter_right  =  right.innerText = ++counter_right
+    localStorage.counter_series = series.innerText = ++counter_series
+
+    window.setTimeout(nextWord, [1200]);
+  }
+  else {
+    arrHitMiss[1] = +arrHitMiss[1]+1
+    splicedWord.miss = arrHitMiss[1]
+    document.body.style.backgroundColor = "red"; 
+    localStorage.counter_wrong = wrong.innerText = ++counter_wrong
+    localStorage.counter_series = series.innerText = counter_series = 0
+
+    input_form.value = ""
+    word.innerText = splicedWord[ru_en]+" = "+lekalo; 
+  }
+  localStorage[splicedWord[0]] = arrHitMiss.join(',')
+
+  hitMiss.innerHTML = `<span id=hit>${splicedWord.hit}</span> :
+  <span id=miss>${splicedWord.miss}</span>`
+}
+
+// TODO если "не помню" не засчитывать ввод по подсказке как hit
